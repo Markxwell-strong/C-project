d3964a0025bdcbbcf63773e03eac01f63719dacf
markxwell-strong <cleandemon5@gmail.com>
2024-09-14 23:27:13 +0300

    remove some text

--- README.md
+++ README.md
@@ -1,5 +1,2 @@
-print('hello world')
-box one = ('567')
-box two = ('786')
-print ('box one + box two')is
+print('Am Makxwell wanjau')
     # visionaries
---
+++ main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+    printf("Hello Mercy!\n");
+    printf("My day is fine am doing just great and enjoying\n");
+    printf("How is your day");
+    return 0;
+}

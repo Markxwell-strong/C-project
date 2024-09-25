b5ee7080fdfd0ab3e072ed02fc4b36a2741c6d18
markxwell-strong <cleandemon5@gmail.com>
2024-09-25 11:06:38 +0300

    update the project

--- quiz1.c
+++ quiz1.c
@@ -5,19 +5,19 @@ Date of coding:9/23/2024*/
 #include <stdio.h>
 int main() {
     float P;//This is the principal amount
-    int T;//This is time for the investment
+    float T;//This is time for the investment
     float r;//This is the rate of intrest per period of investment
     float SI;//This is the intrest earned per period
     printf("Please enter the principal Amount\n");
     scanf("%f",&P);
     printf("Please enter the time taken for the investment\n");
-    scanf("%d",&T);
+    scanf("%f",&T);
     printf("Please enter the rate of intrest in the investment\n");
     scanf("%f",&r);
     SI=(P*T*r)/100;
     r=r/100;
     printf("You entered principal amount as %f\n",P);
-    printf("You entered time for investment as %d\n",T);
+    printf("You entered time for investment as %f\n",T);
     printf("You entered rate of the simple interst as %f\n",r*100);
     printf("You entered simple intrest earned as %f\n",SI);
     return 0;
--- quiz2.c
+++ quiz2.c
@@ -7,7 +7,7 @@ Date of coding 9/24/2024*/
 int main() {
     float P;//This is the principal amount
     float n;//number of compounding periods per year
-    int t;//Total number of years
+    float t;//Total number of years
     float r;//Intest rate
     float A;//This is the final amount including intrest
     printf("Please enter the principal amount\n");
@@ -15,7 +15,7 @@ int main() {
     printf("Please enter number of compounding perionds per year\n");
     scanf("%f",&n);
     printf("Please enter the number of years invested\n");
-    scanf("%d",&t);
+    scanf("%f",&t);
     printf("Please enter the rate of interst\n");
     scanf("%f",&r);
     r=r/100;
@@ -23,7 +23,7 @@ int main() {
     printf("You entered %f as the principal amount\n",P);
     printf("You entered %f as the number of compounding periods per year\n",n);
     printf("You entered %f as the interest rate\n",r*100);
-    printf("You entered %d as the total number of years invested\n",t);
+    printf("You entered %f as the total number of years invested\n",t);
     printf("Therefore your final amount is %f\n",A);
     return 0;
 }
